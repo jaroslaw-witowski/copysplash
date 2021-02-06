0ed2aaa09831e47b20f7d10223c1417496e13f2e
jaroslaw-witowski <jar.witowski@gmail.com>
2021-02-06 22:27:02 +0100

    5-modal: added SVG's for modal. Added location display into modal

--- frontend/src/components/modal/VerticalModal.tsx
+++ frontend/src/components/modal/VerticalModal.tsx
@@ -1,34 +1,48 @@
 import { Modal } from "react-bootstrap";
+import SVGLoader from "../svgloader/SVGLoader";
 import "./verticalmodal.css";
+import user from '../../assets/user.svg';
+import locationPin from '../../assets/pin.svg';
 
-const VerticalModal: React.FC<any> = ({modalInfo, show, onHide}) => {
-  const author = 'Author';
-
+const VerticalModal: React.FC<any> = ({ modalInfo, show, onHide }) => {
+  const author = "Author";
+  const location = "Location";
 
   return (
     modalInfo && (
       <Modal
-        {...{show, onHide}}
+        {...{ show, onHide }}
         dialogClassName={"primaryModal"}
         aria-labelledby="contained-modal-title-vcenter"
         centered
         animation={false}
-
       >
         <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            <a href={modalInfo.user.links.html} target='./'>
+          <div className="modal-header">
+            <a href={modalInfo.user.links.html} target="./">
               <img
                 className="modal-profil-picture"
                 src={modalInfo.user.profile_image.medium}
                 alt="profile"
               />
             </a>
-          </Modal.Title>
-          <div className='modal-author'>
-            <p className='modal-author-name'>{author}</p>
-            <p className='modal-profile-name'>{modalInfo.user.name}</p>
+            <div className="modal-author">
+              <p className="modal-author-title">
+              <SVGLoader imageAdress={user} alt={'modal-author-icon'} className={'modal-author-icon'}/>
+                {author}
+                </p>
+              <p className="modal-profile-name">{modalInfo.user.name}</p>
+            </div>
           </div>
+          {modalInfo.user.location && (
+            <div className="modal-location">
+              <p className="modal-location-title">
+                <SVGLoader imageAdress={locationPin} alt={'modal-location-icon'} className={'modal-location-icon'}/>
+                {location}
+              </p>
+              <p className="modal-location-name">{modalInfo.user.location}</p>
+            </div>
+          )}
         </Modal.Header>
         <Modal.Body>
           <img

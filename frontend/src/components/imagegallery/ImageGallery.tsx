e9d077be552df0e9f4078ab618a6218e5a04a973
jaroslaw-witowski <65184231+jaroslaw-witowski@users.noreply.github.com>
2021-02-07 15:14:39 +0100

    5-modal (#6)
    * 5-modal: Created basic modal display in ImageGallery
    
    * Modified modal logic. Responsive design added
    
    * 5-modal: fixed modal props console error
    
    * 5-modal: Fixed imageGallery styling
    
    * 5-modal: added SVG's for modal. Added location display into modal

--- frontend/src/components/imagegallery/ImageGallery.tsx
+++ frontend/src/components/imagegallery/ImageGallery.tsx
@@ -1,27 +1,44 @@
+import { useState, useEffect } from "react";
 import "./imagegallery.css";
+import VerticalModal from "../modal/VerticalModal";
 
 const ImageGallery: React.FC<any> = ({ imageGallery }) => {
+  const [modalShow, setModalShow] = useState(false);
+  const [modalInfo, setModalInfo] = useState<any>(null);
+
+  useEffect(() => {
+    setModalShow(true);
+  }, [modalInfo]);
+
   return (
     <div className="masonry-wrapper">
       <div className="masonry">
         {imageGallery.map((image: any) => {
           return (
-            <a
+            <button
               key={image.id}
-              href={`imageGallery/${image.id}`}
-              className="image-gallery-link"
+              onClick={() => {
+                setModalInfo(image);
+              }}
+              className="image-gallery-button-link masonry-item"
             >
-              <div className="masonry-item">
                 <img
                   className="masonry-content"
                   alt={image.alt_description}
                   src={image.urls.small}
                 />
-              </div>
-            </a>
+            </button>
           );
         })}
       </div>
+      <VerticalModal
+        modalInfo={modalInfo}
+        show={modalShow}
+        onHide={() => {
+          setModalShow(false);
+          setModalInfo(null);
+        }}
+      />
     </div>
   );
 };
